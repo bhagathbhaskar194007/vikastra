ee3f9f1c510eba4c3644312745a8c3d8770adac9
D Bhagath Bhaskar Unnithan <bhagathbhaskar194007@gmail.com>
2026-06-22 15:10:26 +0530

    Update Team.jsx

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -52,7 +52,7 @@ const members = [
     badge: "Technology",
     socialLinks: {
       instagram: "https://www.instagram.com/_sa.lman__?igsh=ZXhkbmJsbWFmZ2l6",
-      x:"https://x.com/SalmanFarizA",
+      x:"https://x.net/SalmanFarizA",
       threads:"https://www.threads.com/@_sa.lman__",
       Linkedin:"https://www.linkedin.com/in/d-bhagath-bhaskar-unnithan-6b8a713a5?utm_source=share_via&utm_content=profile&utm_medium=member_android"
     }
@@ -69,7 +69,7 @@ const members = [
     socialLinks: {
       instagram: "https://www.instagram.com/_adarshhhh____?igsh=MXhuaWw4ZGQ5bTRvdA==",
       x:"https://x.com/Bhagath_194_007",
-      threads:"https://www.threads.com/@_adarshhhh____",
+      threads:"https://www.threads.net/@_adarshhhh____",
       Linkedin:"https://www.linkedin.com/in/d-bhagath-bhaskar-unnithan-6b8a713a5?utm_source=share_via&utm_content=profile&utm_medium=member_android"
     }
   },
@@ -85,7 +85,7 @@ const members = [
     socialLinks: {
       instagram: "https://www.instagram.com/_snehas_lal_?igsh=azFseXY5d3lzMWtl",
       x:"https://x.com/Bhagath_194_007",
-      threads:"https://www.threads.com/@_snehas_lal_",
+      threads:"https://www.threads.net/@_snehas_lal_",
       Linkedin:"https://www.linkedin.com/in/d-bhagath-bhaskar-unnithan-6b8a713a5?utm_source=share_via&utm_content=profile&utm_medium=member_android"
     }
   } 
